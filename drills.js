ece089865ea24c158511fc0ec8bd670c0906ce42
Scott Schroeder <snschroeder89@gmail.com>
2019-11-05 10:46:34 -0600

    first 4 drills done

---
+++ drills.js
@@ -0,0 +1,8 @@
+/*
+1.1 - O(1) - constant time. Since you are effectively asking everyone at once, your answer will always come back in the same amount of time
+1.2 - O(n) - linear time. Since you are individually querying people, time will scale linearly with the number of the people present
+
+2. - O(1) - constant time. We are running an operation on a single value then returning true or false. 
+3. O(n^2) - polynomial time. We are running n operations on m values, therefore time complexity is n*m, otherwise known as n^2
+4. O(n) - linear time. Time complexity scales lienarly with the size of the array. If the array is 5, 5 operations are run, 10, 10 operations are run, etc.
+*/
